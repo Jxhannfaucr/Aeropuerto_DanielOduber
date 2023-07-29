2afaa8c6ee443d50be24e4eb18762ab924afeeca
Felix Suarez <felix.suarez@ingenuiti.com>
2023-07-29 16:58:53 -0600

    update add login

---
+++ Aeropuerto_DanielOduber/Login.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Login
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Login))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.UsuarioIngreso = New System.Windows.Forms.TextBox()
+        Me.contraseñalogin = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(60, 19)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(133, 141)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Georgia", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(87, 175)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 18)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Usuario:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Georgia", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(77, 229)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(93, 18)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Contraseña:"
+        '
+        'UsuarioIngreso
+        '
+        Me.UsuarioIngreso.Location = New System.Drawing.Point(60, 196)
+        Me.UsuarioIngreso.Name = "UsuarioIngreso"
+        Me.UsuarioIngreso.Size = New System.Drawing.Size(133, 20)
+        Me.UsuarioIngreso.TabIndex = 3
+        '
+        'contraseñalogin
+        '
+        Me.contraseñalogin.Location = New System.Drawing.Point(60, 260)
+        Me.contraseñalogin.Name = "contraseñalogin"
+        Me.contraseñalogin.Size = New System.Drawing.Size(133, 20)
+        Me.contraseñalogin.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button1.Location = New System.Drawing.Point(69, 310)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(114, 36)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Ingresar"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.contraseñalogin)
+        Me.GroupBox1.Controls.Add(Me.UsuarioIngreso)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(256, 112)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(255, 395)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(824, 644)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Login"
+        Me.Text = "Login"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents UsuarioIngreso As TextBox
+    Friend WithEvents contraseñalogin As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ Aeropuerto_DanielOduber/Login.vb
@@ -0,0 +1,36 @@
+﻿Public Class Login
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.StartPosition = FormStartPosition.Manual ' Se establece la posición manualmente
+
+        ' Se establece la posición inicial del formulario en las coordenadas deseadas
+        Me.Location = New Point(650, 170) ' Aqui pusimos lo que fue la posicion inicial de la ventana login
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim usuario As String = UsuarioIngreso.Text
+        Dim contrasena As String = contraseñalogin.Text
+
+        If usuario.Equals("admin") AndAlso contrasena.Equals("johan_gay") Then
+
+            Dim pasar_Al_Menu_admin As New Form_Menu()
+
+            pasar_Al_Menu_admin.StartPosition = FormStartPosition.Manual ' Se establece la posición manualmente
+            ' Se establece la posición inicial del formulario en las coordenadas deseadas
+            pasar_Al_Menu_admin.Location = New Point(200, 170) ' Aqui pusimos lo que fue la posicion inicial de la ventana login
+            pasar_Al_Menu_admin.Show()
+            Me.Hide()
+
+        ElseIf usuario.Equals("usuario") AndAlso contrasena.Equals("yosiney_Guapo") Then
+            Dim pasar_Al_Menu_usuario As New Form_Menu()
+
+            pasar_Al_Menu_usuario.StartPosition = FormStartPosition.Manual ' Se establece la posición manualmente
+            ' Se establece la posición inicial del formulario en las coordenadas deseadas
+            pasar_Al_Menu_usuario.Location = New Point(200, 170) ' Aqui pusimos lo que fue la posicion inicial de la ventana login
+            pasar_Al_Menu_usuario.Show()
+            Me.Hide()
+        Else
+            MessageBox.Show("Usuario o contraseña incorrectos. Inténtalo nuevamente.")
+        End If
+    End Sub
+End Class
