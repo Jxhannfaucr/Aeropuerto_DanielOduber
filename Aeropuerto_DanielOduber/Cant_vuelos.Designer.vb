6cdc864b13ef6c545e57ce69be2d024702d79404
Felix Suarez <felix.suarez@ingenuiti.com>
2023-08-06 20:30:28 -0600

    Update, creation the datagrid

--- Aeropuerto_DanielOduber/Cant_vuelos.Designer.vb
+++ Aeropuerto_DanielOduber/Cant_vuelos.Designer.vb
@@ -103,7 +103,7 @@ Partial Class Cant_vuelos
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Location = New System.Drawing.Point(371, 222)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(386, 167)
+        Me.DataGridView1.Size = New System.Drawing.Size(243, 111)
         Me.DataGridView1.TabIndex = 7
         '
         'Cant_vuelos
--- Aeropuerto_DanielOduber/Cant_vuelos.vb
+++ Aeropuerto_DanielOduber/Cant_vuelos.vb
@@ -6,22 +6,29 @@ Public Class Cant_vuelos
     Public conect As Conexion_BD = New Conexion_BD
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim Busqueda As String = "SELECT count(IDPasajero) AS CantVuelos " &
-                        "FROM TblPasajero " &
-                        "INNER JOIN Ventanillas ON TblPasajero.ID_Ventanilla = Ventanillas.Id_Proceso " &
-                        "WHERE Ventanillas.NumeroVentanilla = @NumeroVentanilla AND Ventanillas.Fecha = @Fecha"
+        Dim Busqueda As String = "SELECT COUNT(IDPasajero) AS CantVuelos, Destino " &
+                    "FROM TblPasajero " &
+                    "INNER JOIN Ventanillas ON TblPasajero.ID_Ventanilla = Ventanillas.Id_Proceso " &
+                    "WHERE Ventanillas.NumeroVentanilla = @NumeroVentanilla AND Ventanillas.Fecha = @Fecha " &
+                    "GROUP BY Destino"
 
         Dim cmd As SqlCommand = New SqlCommand(Busqueda, conect.Conectar())
         cmd.Parameters.AddWithValue("@NumeroVentanilla", List_ventanillas.Text)
-        cmd.Parameters.AddWithValue("@Fecha", buscar_fecha.Text) ' Aquí utilizamos buscar_fecha.Value para obtener el valor DateTime.
-        Dim cantVuelos As Integer = Convert.ToInt32(cmd.ExecuteScalar()) ' Utilizamos ExecuteScalar para obtener un solo valor (la suma de IDPasajero).
+        cmd.Parameters.AddWithValue("@Fecha", buscar_fecha.Text)
 
+        Dim dt As New DataTable()
+        Dim da As New SqlDataAdapter(cmd)
 
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
 
-        Dim resultado_cantidad As String = Convert.ToString(cmd.ExecuteScalar()) ' proceso de una consulta
-        Total_de_vuelos.Text = resultado_cantidad
+        ' Mostrar el total de vuelos en el TextBox
+        Dim cantVuelos As Integer = dt.Rows.Cast(Of DataRow)().Sum(Function(row) Convert.ToInt32(row("CantVuelos")))
+        Total_de_vuelos.Text = cantVuelos.ToString()
+
         conect.Cerrar()
     End Sub
+
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Dim volver_menu_informes As New Informes()
