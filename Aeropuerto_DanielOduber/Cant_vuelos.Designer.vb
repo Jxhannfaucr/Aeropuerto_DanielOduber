fe8a4b03db1c89eeb403e3efa288852f0792fd68
Felix Suarez <felix.suarez@ingenuiti.com>
2023-08-23 20:59:17 -0600

    update, Algo que no puo hacer Dylan

--- Aeropuerto_DanielOduber/Cant_vuelos.Designer.vb
+++ Aeropuerto_DanielOduber/Cant_vuelos.Designer.vb
@@ -30,39 +30,46 @@ Partial Class Cant_vuelos
         Me.Total_de_vuelos = New System.Windows.Forms.TextBox()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.DataGridView4 = New System.Windows.Forms.DataGridView()
+        Me.DataGridView3 = New System.Windows.Forms.DataGridView()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
+        CType(Me.DataGridView4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'buscar_fecha
         '
         Me.buscar_fecha.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.buscar_fecha.Location = New System.Drawing.Point(8, 50)
-        Me.buscar_fecha.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.buscar_fecha.Location = New System.Drawing.Point(6, 41)
         Me.buscar_fecha.Name = "buscar_fecha"
-        Me.buscar_fecha.Size = New System.Drawing.Size(427, 30)
+        Me.buscar_fecha.Size = New System.Drawing.Size(321, 26)
         Me.buscar_fecha.TabIndex = 0
         '
         'List_ventanillas
         '
         Me.List_ventanillas.FormattingEnabled = True
         Me.List_ventanillas.Items.AddRange(New Object() {"1", "2", "3", "4", "Todos"})
-        Me.List_ventanillas.Location = New System.Drawing.Point(135, 105)
-        Me.List_ventanillas.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.List_ventanillas.Location = New System.Drawing.Point(211, 165)
         Me.List_ventanillas.Name = "List_ventanillas"
-        Me.List_ventanillas.Size = New System.Drawing.Size(160, 24)
+        Me.List_ventanillas.Size = New System.Drawing.Size(121, 21)
         Me.List_ventanillas.TabIndex = 1
         '
         'Button1
         '
-        Me.Button1.Enabled = False
         Me.Button1.Font = New System.Drawing.Font("Georgia", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(165, 156)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button1.Location = New System.Drawing.Point(123, 163)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(100, 28)
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "Buscar"
         Me.Button1.UseVisualStyleBackColor = True
@@ -71,85 +78,168 @@ Partial Class Cant_vuelos
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Georgia", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(35, 235)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Location = New System.Drawing.Point(26, 191)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(194, 23)
+        Me.Label1.Size = New System.Drawing.Size(149, 18)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "Cantidad de destinos:"
         '
         'Total_de_vuelos
         '
-        Me.Total_de_vuelos.Location = New System.Drawing.Point(241, 235)
-        Me.Total_de_vuelos.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Total_de_vuelos.Location = New System.Drawing.Point(181, 192)
         Me.Total_de_vuelos.Name = "Total_de_vuelos"
-        Me.Total_de_vuelos.Size = New System.Drawing.Size(132, 22)
+        Me.Total_de_vuelos.Size = New System.Drawing.Size(100, 20)
         Me.Total_de_vuelos.TabIndex = 4
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(916, 23)
-        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button2.Location = New System.Drawing.Point(804, 19)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(100, 28)
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 5
         Me.Button2.Text = "Regresar"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.DataGridView1)
-        Me.GroupBox1.Controls.Add(Me.buscar_fecha)
+        Me.GroupBox1.Controls.Add(Me.DataGridView4)
+        Me.GroupBox1.Controls.Add(Me.DataGridView3)
         Me.GroupBox1.Controls.Add(Me.List_ventanillas)
         Me.GroupBox1.Controls.Add(Me.Total_de_vuelos)
+        Me.GroupBox1.Controls.Add(Me.DataGridView2)
+        Me.GroupBox1.Controls.Add(Me.CheckBox5)
+        Me.GroupBox1.Controls.Add(Me.CheckBox4)
+        Me.GroupBox1.Controls.Add(Me.CheckBox3)
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Controls.Add(Me.DataGridView1)
+        Me.GroupBox1.Controls.Add(Me.buscar_fecha)
         Me.GroupBox1.Controls.Add(Me.Button1)
         Me.GroupBox1.Controls.Add(Me.Label1)
-        Me.GroupBox1.Location = New System.Drawing.Point(331, 73)
-        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 59)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.GroupBox1.Size = New System.Drawing.Size(465, 457)
+        Me.GroupBox1.Size = New System.Drawing.Size(867, 379)
         Me.GroupBox1.TabIndex = 6
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Elige fecha y vuelo"
+        '
+        'DataGridView4
+        '
+        Me.DataGridView4.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView4.Location = New System.Drawing.Point(658, 235)
+        Me.DataGridView4.Name = "DataGridView4"
+        Me.DataGridView4.RowHeadersWidth = 62
+        Me.DataGridView4.Size = New System.Drawing.Size(209, 111)
+        Me.DataGridView4.TabIndex = 15
+        '
+        'DataGridView3
+        '
+        Me.DataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView3.Location = New System.Drawing.Point(436, 235)
+        Me.DataGridView3.Name = "DataGridView3"
+        Me.DataGridView3.RowHeadersWidth = 62
+        Me.DataGridView3.Size = New System.Drawing.Size(209, 111)
+        Me.DataGridView3.TabIndex = 14
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(221, 235)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.RowHeadersWidth = 62
+        Me.DataGridView2.Size = New System.Drawing.Size(209, 111)
+        Me.DataGridView2.TabIndex = 13
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.Location = New System.Drawing.Point(123, 110)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(56, 17)
+        Me.CheckBox5.TabIndex = 12
+        Me.CheckBox5.Text = "Todos"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(35, 110)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox4.TabIndex = 11
+        Me.CheckBox4.Text = "Ventanilla 4"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(211, 86)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox3.TabIndex = 10
+        Me.CheckBox3.Text = "Ventanilla 3"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(123, 86)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox2.TabIndex = 9
+        Me.CheckBox2.Text = "Ventanilla 2"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(35, 86)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox1.TabIndex = 8
+        Me.CheckBox1.Text = "Ventanilla 1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
         '
         'DataGridView1
         '
         Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(35, 290)
-        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.DataGridView1.Location = New System.Drawing.Point(6, 235)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowHeadersWidth = 62
-        Me.DataGridView1.Size = New System.Drawing.Size(400, 137)
+        Me.DataGridView1.Size = New System.Drawing.Size(209, 111)
         Me.DataGridView1.TabIndex = 7
         '
         'Label3
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Georgia", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(373, 23)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Location = New System.Drawing.Point(280, 19)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(401, 24)
+        Me.Label3.Size = New System.Drawing.Size(315, 18)
         Me.Label3.TabIndex = 8
         Me.Label3.Text = "Informe de vuelos vendidos por ventanillas."
         '
         'Cant_vuelos
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.ClientSize = New System.Drawing.Size(1067, 554)
+        Me.ClientSize = New System.Drawing.Size(903, 450)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Button2)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Cant_vuelos"
         Me.Text = "Cantidad de vuelos vendidos"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
+        CType(Me.DataGridView4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
@@ -165,4 +255,12 @@ Partial Class Cant_vuelos
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents Label3 As Label
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents DataGridView4 As DataGridView
+    Friend WithEvents DataGridView3 As DataGridView
+    Friend WithEvents DataGridView2 As DataGridView
 End Class
--- Aeropuerto_DanielOduber/Cant_vuelos.vb
+++ Aeropuerto_DanielOduber/Cant_vuelos.vb
@@ -5,35 +5,134 @@ Public Class Cant_vuelos
 
     Public conect As Conexion_BD = New Conexion_BD
 
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim ventanilla_seleccionada As String = List_ventanillas.SelectedItem.ToString
+
+
+
+        Dim cantV1 As Integer = 0
+        Dim cantV2 As Integer = 0
+        Dim cantV3 As Integer = 0
+        Dim cantV4 As Integer = 0
+
         Try
-            If Not ventanilla_seleccionada = "Todos" Then
 
 
-                Dim Busqueda As String = "SELECT COUNT(IDPasajero) AS CantVuelos, Destino " &
-                    "FROM TblPasajero " &
-                    "INNER JOIN Ventanillas ON TblPasajero.ID_Ventanilla = Ventanillas.Id_Proceso " &
-                    "WHERE Ventanillas.NumeroVentanilla = @NumeroVentanilla AND Ventanillas.Fecha = @Fecha " &
-                    "GROUP BY Destino"
+            If Not CheckBox5.Checked Then
+                If CheckBox1.Checked Then
+                    Dim Busqueda1 As String = "SELECT COUNT(IDPasajero) AS CantVuelos, Destino " &
+                   "FROM TblPasajero " &
+                   "INNER JOIN Ventanillas ON TblPasajero.ID_Ventanilla = Ventanillas.Id_Proceso " &
+                   "WHERE Ventanillas.NumeroVentanilla = 1 AND Ventanillas.Fecha = @Fecha " &
+                   "GROUP BY Destino"
 
-                Dim cmd As SqlCommand = New SqlCommand(Busqueda, conect.Conectar())
-                cmd.Parameters.AddWithValue("@NumeroVentanilla", ventanilla_seleccionada)
-                cmd.Parameters.AddWithValue("@Fecha", buscar_fecha.Text)
+                    Dim cmd1 As SqlCommand = New SqlCommand(Busqueda1, conect.Conectar())
+                    cmd1.Parameters.AddWithValue("@Fecha", buscar_fecha.Text)
 
-                Dim dt As New DataTable()
-                Dim da As New SqlDataAdapter(cmd)
+                    Dim dt1 As New DataTable()
+                    Dim da1 As New SqlDataAdapter(cmd1)
 
-                da.Fill(dt)
-                DataGridView1.DataSource = dt
+                    da1.Fill(dt1)
+                    DataGridView1.DataSource = dt1
 
-                ' Mostrar el total de vuelos en el TextBox
-                Dim cantVuelos As Integer = dt.Rows.Cast(Of DataRow)().Sum(Function(row) Convert.ToInt32(row("CantVuelos")))
-                Total_de_vuelos.Text = cantVuelos.ToString()
+                    ' Mostrar el total de vuelos en el TextBox
+                    cantV1 = dt1.Rows.Cast(Of DataRow)().Sum(Function(row) Convert.ToInt32(row("CantVuelos")))
 
-                conect.Cerrar()
+                    conect.Cerrar()
+                Else
+                    Dim dt1 As New DataTable()
+                    DataGridView1.DataSource = dt1
+                End If
 
-            Else
+                If CheckBox2.Checked Then
+                    Dim Busqueda2 As String = "SELECT COUNT(IDPasajero) AS CantVuelos, Destino " &
+                   "FROM TblPasajero " &
+                   "INNER JOIN Ventanillas ON TblPasajero.ID_Ventanilla = Ventanillas.Id_Proceso " &
+                   "WHERE Ventanillas.NumeroVentanilla = 2 AND Ventanillas.Fecha = @Fecha " &
+                   "GROUP BY Destino"
+
+                    Dim cmd2 As SqlCommand = New SqlCommand(Busqueda2, conect.Conectar())
+                    cmd2.Parameters.AddWithValue("@Fecha", buscar_fecha.Text)
+
+                    Dim dt2 As New DataTable()
+                    Dim da2 As New SqlDataAdapter(cmd2)
+
+                    da2.Fill(dt2)
+                    DataGridView2.DataSource = dt2
+
+                    ' Mostrar el total de vuelos en el TextBox
+                    cantV2 = dt2.Rows.Cast(Of DataRow)().Sum(Function(row) Convert.ToInt32(row("CantVuelos")))
+
+                    conect.Cerrar()
+                Else
+                    Dim dt2 As New DataTable()
+                    DataGridView2.DataSource = dt2
+                End If
+
+                If CheckBox3.Checked Then
+                    Dim Busqueda3 As String = "SELECT COUNT(IDPasajero) AS CantVuelos, Destino " &
+                   "FROM TblPasajero " &
+                   "INNER JOIN Ventanillas ON TblPasajero.ID_Ventanilla = Ventanillas.Id_Proceso " &
+                   "WHERE Ventanillas.NumeroVentanilla = 3 AND Ventanillas.Fecha = @Fecha " &
+                   "GROUP BY Destino"
+
+                    Dim cmd3 As SqlCommand = New SqlCommand(Busqueda3, conect.Conectar())
+                    cmd3.Parameters.AddWithValue("@Fecha", buscar_fecha.Text)
+
+                    Dim dt3 As New DataTable()
+                    Dim da3 As New SqlDataAdapter(cmd3)
+
+                    da3.Fill(dt3)
+                    DataGridView3.DataSource = dt3
+
+                    ' Mostrar el total de vuelos en el TextBox
+                    cantV3 = dt3.Rows.Cast(Of DataRow)().Sum(Function(row) Convert.ToInt32(row("CantVuelos")))
+
+                    conect.Cerrar()
+                Else
+                    Dim dt3 As New DataTable()
+                    DataGridView3.DataSource = dt3
+                End If
+
+                If CheckBox4.Checked Then
+                    Dim Busqueda4 As String = "SELECT COUNT(IDPasajero) AS CantVuelos, Destino " &
+                   "FROM TblPasajero " &
+                   "INNER JOIN Ventanillas ON TblPasajero.ID_Ventanilla = Ventanillas.Id_Proceso " &
+                   "WHERE Ventanillas.NumeroVentanilla = 4 AND Ventanillas.Fecha = @Fecha " &
+                   "GROUP BY Destino"
+
+                    Dim cmd4 As SqlCommand = New SqlCommand(Busqueda4, conect.Conectar())
+                    cmd4.Parameters.AddWithValue("@Fecha", buscar_fecha.Text)
+
+                    Dim dt4 As New DataTable()
+                    Dim da4 As New SqlDataAdapter(cmd4)
+
+                    da4.Fill(dt4)
+                    DataGridView4.DataSource = dt4
+
+                    ' Mostrar el total de vuelos en el TextBox
+                    cantV4 = dt4.Rows.Cast(Of DataRow)().Sum(Function(row) Convert.ToInt32(row("CantVuelos")))
+
+                    conect.Cerrar()
+                Else
+                    Dim dt4 As New DataTable()
+                    DataGridView4.DataSource = dt4
+                End If
+
+                Dim sumatotalventanillas As Integer = cantV1 + cantV2 + cantV3 + cantV4
+                Total_de_vuelos.Text = sumatotalventanillas
+
+            ElseIf CheckBox5.Checked Then
+
+                Dim dt1 As New DataTable()
+                DataGridView1.DataSource = dt1
+                Dim dt2 As New DataTable()
+                DataGridView2.DataSource = dt2
+                Dim dt3 As New DataTable()
+                DataGridView3.DataSource = dt3
+                Dim dt4 As New DataTable()
+                DataGridView4.DataSource = dt4
+
                 Dim Busqueda As String = "SELECT COUNT(IDPasajero) AS CantVuelos, Destino
                                                 FROM TblPasajero
                                                 INNER JOIN Ventanillas ON TblPasajero.ID_Ventanilla = Ventanillas.Id_Proceso
@@ -80,8 +179,25 @@ Public Class Cant_vuelos
 
     End Sub
 
-    Private Sub List_ventanillas_SelectedIndexChanged(sender As Object, e As EventArgs) Handles List_ventanillas.SelectedIndexChanged
-        Button1.Enabled = True
-    End Sub
 
+
+    Private Sub CheckBox5_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox5.CheckedChanged
+        If CheckBox5.Checked Then
+            CheckBox1.Enabled = False
+            CheckBox2.Enabled = False
+            CheckBox3.Enabled = False
+            CheckBox4.Enabled = False
+
+            CheckBox1.Checked = False
+            CheckBox2.Checked = False
+            CheckBox3.Checked = False
+            CheckBox4.Checked = False
+
+        Else
+            CheckBox1.Enabled = True
+            CheckBox2.Enabled = True
+            CheckBox3.Enabled = True
+            CheckBox4.Enabled = True
+        End If
+    End Sub
 End Class
